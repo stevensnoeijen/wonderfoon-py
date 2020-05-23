159fccb7bfd69dfcfde6a3198678a2319432bed8
steven snoeijen <stevensnoeijen@gmail.com>
2020-05-23 16:54:29 +0200

    fixed multiple dailtones fixed to pause dailtone while dailing or playing music

--- t65.py
+++ t65.py
@@ -4,55 +4,66 @@ import gpiozero
 from signal import pause
 import simpleaudio as sa
 import time
+import threading
 
 import faulthandler
 faulthandler.enable()
 
 class T65:
     unhooked = False
-    playing = None
+    playingMusic = None
 
     def run(self):
-        hook = gpiozero.Button(16) # TODO: replace with config
-        hook.when_released = self.offHook
-        hook.when_pressed = self.onHook
-
         self.rotator = Rotator()
         self.rotator.when_dailed = self.dailed
 
+        self.hook = gpiozero.Button(16) # TODO: replace with config
+        self.hook.when_released = self.offHook
+        self.hook.when_pressed = self.onHook
+        if not self.hook.is_pressed: # start offHook when it's already of the hook
+            self.offHook()
+
+        threading.Thread(name='dailer', target=self.playDailTone).start()
         print('t65 started')
         pause()
 
     def onHook(self):
+        print('on hook')
         self.unhooked = False
 
     def playDailTone(self):
         dailTone = sa.WaveObject.from_wave_file('music/kiestoon-2s.wav')
 
-        while(self.unhooked):
+        while(True):
+            if not self.unhooked:
+                time.sleep(1)
+
+            if self.playingMusic or self.rotator.isDailing:
+                time.sleep(.5)
+                continue
+
             play = dailTone.play()
             play.wait_done()
-            play.stop()
-            del play
 
     def offHook(self):
+        print('off hook')
         self.unhooked = True
-        self.playDailTone()
 
     def dailed(self, number):
         self.playMusic(str(number) + '.wav')
 
     def playMusic(self, song):
-        if(self.playing):
-            self.playing.stop()
-            del self.playing
+        if self.playingMusic:
+            self.playingMusic.stop()
+            del self.playingMusic
 
-        audio = sa.WaveObject.from_wave_file('music/' + song)
-        self.playing = audio.play()
-        self.playing.wait_done()
-        del self.playing
+        music = sa.WaveObject.from_wave_file('music/' + song)
+        self.playingMusic = music.play()
+        self.playingMusic.wait_done()
+        del self.playingMusic
 
 class Rotator:
+    isDailing = False
     count = -1 # starts at -1 because steps is trigged twice at the start
     lastNumber = -1
     __when_dailed = ...
@@ -62,12 +73,21 @@ class Rotator:
         self.step.when_pressed = self.step_pressed
 
         self.ison = gpiozero.Button(26) # TODO: replace with config
+        self.ison.when_pressed = self.ison_pressed
         self.ison.when_released = self.ison_released
     
     def step_pressed(self):
         self.count += 1
 
+    def ison_pressed(self):
+        self.isDailing = True
+
     def ison_released(self):
+        if self.count <= 0:
+            self.count = -1
+            return
+
+        self.isDailing = False
         self.lastNumber = self.count
         self.count = -1 # reset counter
         self.dailed(self.lastNumber)
@@ -82,7 +102,7 @@ class Rotator:
 
     def dailed(self, number):
         print('dailed ' + str(number))
-        if(self.when_dailed):
+        if self.when_dailed:
             self.when_dailed(number)
 
 
