7151f3f96df1e6df6ce4a5caa9537b117ecbf4de
steven snoeijen <stevensnoeijen@gmail.com>
2020-05-23 15:26:57 +0200

    added kiestoon and kiestoon-2s in wav added Rotator class to with when_dailed callback added documentation how to install implemented dailtone and playing music

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
 # wonderfoon-py
-python implementatie voor wonderfoon.nl 1.4
+Python implementatie voor wonderfoon.nl 1.4.
+
+Waarom Python? Omdat ik het project werkend wil krijgen op een pi zero en Python wil leren.
 
 __Dit project is in ontwikkeling__
 
 Ik ben niet de eigenaar van wonderfoon.nl, dit project is ge-forked op wonderfoon 1.4 en implementeerd de software in python.
 
-Manual voor http://wonderfoon.nl/WONDERFOON-v1.4.pdf
+> Manual voor http://wonderfoon.nl/WONDERFOON-v1.4.pdf
+
+## Run
+
+### Installatie
+
+https://wolfpaulus.com/raspberry-pi-zero-w-w-python-3/
+
+`alias python='/usr/bin/python3.4'`
+
+```
+pip3 install --upgrade pip setuptools
+pip3 install wheel
+pip3 install pydub
+pip3 install ffmpeg-python
+sudo apt-get install -y python3-dev libasound2-dev
+pip3 install simpleaudio
+```
+
+### Development
+
+Ik ontwikkel op het moment het script lokaal en sync dir naar de pi.
+Op de pi loopt een script om het python script te herstarten als het bestand word opgeslagen:
+
+`while inotifywait -e close_write t65.py; do python3 ./t65.py; done`
+
+> install https://github.com/inotify-tools/inotify-tools/wiki
--- t65.py
+++ t65.py
@@ -2,41 +2,90 @@ from pydub import AudioSegment
 from pydub.playback import play
 import gpiozero
 from signal import pause
-#import time
+import simpleaudio as sa
+import time
+
+import faulthandler
+faulthandler.enable()
 
 class T65:
-    count = -1 # starts at -1 because steps is trigged twice at the start
-    triggered = False
+    unhooked = False
+    playing = None
 
-    def run(self):        
-        step = gpiozero.Button(19)
-        step.when_pressed = self.step_pressed
+    def run(self):
+        hook = gpiozero.Button(16) # TODO: replace with config
+        hook.when_released = self.offHook
+        hook.when_pressed = self.onHook
 
-        ison = gpiozero.Button(26)
-        ison.when_pressed = self.ison_pressed
+        self.rotator = Rotator()
+        self.rotator.when_dailed = self.dailed
 
-        # kiestoon = AudioSegment.from_ogg("kiestoon.ogg")
-        # play(kiestoon)
-
-        #while True:
-        #    print(count)
-        #    time.sleep(1)
+        print('t65 started')
         pause()
 
+    def onHook(self):
+        self.unhooked = False
+
+    def playDailTone(self):
+        dailTone = sa.WaveObject.from_wave_file('music/kiestoon-2s.wav')
+
+        while(self.unhooked):
+            play = dailTone.play()
+            play.wait_done()
+            play.stop()
+            del play
+
+    def offHook(self):
+        self.unhooked = True
+        self.playDailTone()
+
+    def dailed(self, number):
+        self.playMusic(str(number) + '.wav')
+
+    def playMusic(self, song):
+        if(self.playing):
+            self.playing.stop()
+            del self.playing
+
+        audio = sa.WaveObject.from_wave_file('music/' + song)
+        self.playing = audio.play()
+        self.playing.wait_done()
+        del self.playing
+
+class Rotator:
+    count = -1 # starts at -1 because steps is trigged twice at the start
+    lastNumber = -1
+    __when_dailed = ...
+
+    def __init__(self):
+        self.step = gpiozero.Button(19) # TODO: replace with config
+        self.step.when_pressed = self.step_pressed
+
+        self.ison = gpiozero.Button(26) # TODO: replace with config
+        self.ison.when_released = self.ison_released
+    
     def step_pressed(self):
         self.count += 1
-        print("step pressed! " + str(self.count))
 
-    def ison_pressed(self):
-        print("ison pressed!")
-        if False:
-            sound = AudioSegment.from_file("1.wav", format="wav")
-            play(sound)
+    def ison_released(self):
+        self.lastNumber = self.count
+        self.count = -1 # reset counter
+        self.dailed(self.lastNumber)
+
+    @property
+    def when_dailed(self):
+        return self.__when_dailed
+
+    @when_dailed.setter
+    def when_dailed(self, value):
+        self.__when_dailed = value
+
+    def dailed(self, number):
+        print('dailed ' + str(number))
+        if(self.when_dailed):
+            self.when_dailed(number)
 
 
-def main():
+if __name__ == "__main__": 
     t65 = T65()
-    t65.run()
-
-if __name__ == "__main__":
-    main()
+    t65.run() 
